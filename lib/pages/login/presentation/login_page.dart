7b0a4dd2f6a403ca708de2e30142ffd554b5c934
Lorenzo Menghini <lollomenghini@gmail.com>
2024-02-11 20:51:35 +0100

    Add shit on login

--- lib/pages/login/presentation/login_page.dart
+++ lib/pages/login/presentation/login_page.dart
@@ -1,40 +1,56 @@
-import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shatapp/pages/login/presentation/shit_for_login_button.dart';
+import 'package:shatapp/pages/login/presentation/shit_for_login_text.dart';
 import 'package:shatapp/utils/ui_utils/ui_utility.dart';
 
-class LoginPage extends ConsumerWidget with UiUtility {
+class LoginPage extends ConsumerStatefulWidget {
   const LoginPage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends ConsumerState<LoginPage> with UiDimension {
+  double opacity = 1;
+  Alignment alignment = Alignment.bottomCenter;
+
+  void _fadeText() {
+    setState(() {
+      opacity = 0.0;
+    });
+    Future.delayed(const Duration(milliseconds: 100), _moveText);
+  }
+
+  void _moveText() {
+    setState(() {
+      alignment = Alignment.center;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
-      body: Stack(
-        children: [
-          Center(
-            child: AnimatedTextKit(
-              animatedTexts: [
-                TypewriterAnimatedText(
-                  'Hold the shit to start shitting',
-                  cursor: '',
-                  speed: const Duration(milliseconds: 60),
-                  textStyle: context.textTheme.headlineMedium?.copyWith(fontWeight: FontWeight.bold),
-                ),
-              ],
-              totalRepeatCount: 1,
-              displayFullTextOnTap: true,
-              stopPauseOnTap: true,
+      body: Padding(
+        padding: mediumPadding,
+        child: Stack(
+          children: [
+            Center(
+              child: AnimatedOpacity(
+                opacity: opacity,
+                duration: const Duration(milliseconds: 300),
+                child: ShitForLoginText(onFinished: _fadeText),
+              ),
             ),
-          ),
-          const Positioned(
-            bottom: UiDimension.largeSize,
-            left: 0,
-            right: 0,
-            child: ShitForLoginButton(),
-          ),
-        ],
+            AnimatedAlign(
+              alignment: alignment,
+              curve: Curves.easeInOutCubic,
+              duration: const Duration(milliseconds: 300),
+              child: const ShitForLoginButton(),
+            ),
+          ],
+        ),
       ),
     );
   }
